b5e7d13dc51423e533180cd35cc360ecbb7aeff0
mirdan22 <113371529+mirdan22@users.noreply.github.com>
2023-01-16 17:44:46 -0500

    js
    need fix

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 console.log('javaScript is running!');
+// step1
+let bitmapGraphic = document.querySelector("bitmapGraphic");
+
+// step2 
+function logID() {
+    console.log(this.id);
+    // step3
+}
+bitmapGraphic.addEventListener('click', logID);

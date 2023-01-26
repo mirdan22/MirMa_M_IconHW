cd2e56c8acfdc5ec05c94309bd6f082aa9def66a
mirdan22 <113371529+mirdan22@users.noreply.github.com>
2023-01-25 22:19:44 -0500

    readme
    readme updated

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
 console.log('javaScript is running!');
 // step1
-let bitmapGraphic = document.querySelector("bitmapGraphic");
+let dj = document.querySelector("#dj");
+let drum = document.querySelector("#drum");
+let guitar = document.querySelector("#guitar");
+let disk = document.querySelector("#disk");
+let piano = document.querySelector("#piano");
+let headphone = document.querySelector("#headphone");
+let jingle = document.querySelector("#jingle");
+let microphone = document.querySelector("#microphone");
+let phonograph = document.querySelector("#phonograph");
+let speaker = document.querySelector("#speaker");
 
 // step2 
 function logID() {
     console.log('click on this elelment', this.id);
     // step3
 }
-bitmapGraphic.addEventListener('click', logID);
+dj.addEventListener('click', logID);
+drum.addEventListener('click', logID);
+guitar.addEventListener('click', logID);
+disk.addEventListener('click', logID);
+piano.addEventListener('click', logID);
+headphone.addEventListener('click', logID);
+jingle.addEventListener('click', logID);
+microphone.addEventListener('click', logID);
+phonograph.addEventListener('click', logID);
+speaker.addEventListener('click', logID);
+
